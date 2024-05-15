57a7ce79e56976344110c25e488d53ed7721d776
lava <e3663233@yahoo.com.tw>
2024-05-15 14:21:59 +0800

    init controller

---
+++ msit59-vita/Controllers/OrderDetailController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace msit59_vita.Controllers
+{
+    //  後台訂單詳細資訊頁面
+    public class OrderDetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
