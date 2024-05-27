a4989d27e1ea8a9ae9cb23f893f36956d06ecf71
wkeserica1998 <wkeserica1998@gmail.com>
2024-05-27 16:44:10 +0800

    add Order&Comment static file

--- msit59-vita/Controllers/ManagerCommentsController.cs
+++ msit59-vita/Controllers/ManagerCommentsController.cs
@@ -2,9 +2,9 @@
 
 namespace msit59_vita.Controllers
 {
-    public class CommentsManagerController : Controller
+    public class ManagerCommentsController : Controller
     {
-        public IActionResult Index()
+        public IActionResult ManagerComments()
         {
             return View();
         }
