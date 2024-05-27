cccbb99aa5279ab292268d527c4ebc95f5109616
ezMarshall <e3663233@yahoo.com.tw>
2024-05-27 14:58:54 +0800

    query openHours and comments data

--- msit59-vita/Controllers/FavoriteStoresController.cs
+++ msit59-vita/Controllers/FavoriteStoresController.cs
@@ -18,7 +18,7 @@ namespace msit59_vita.Controllers
             _context = context;
         }
 
-
+        
         /*
          * 取得使用者id CustomerID
          * 經由id查找資料 店家資料
@@ -41,7 +41,7 @@ namespace msit59_vita.Controllers
                             s.StorePhoneNumber,
                             s.StoreImage
                         };
-
+            // todo 如何做left join?
             var queryOpeningHours = from o in _context.Favorites
                                     join s in _context.Stores on o.StoreId equals s.StoreId
                                     join so in _context.StoreOpeningHours on o.StoreId equals so.StoreId
@@ -50,17 +50,21 @@ namespace msit59_vita.Controllers
                                         so.Store.StoreId,
                                         so.MyWeekDay,
                                         so.StoreOpenOrNot,
-                                        so.StoreOpeningTime,
-                                        so.StoreClosingTime
+                                        StoreOpeningTime = so.StoreOpeningTime.ToString().Substring(0,5).Replace(":",""),
+                                        StoreClosingTime = so.StoreClosingTime.ToString().Substring(0,5).Replace(":",""),
                                     };
+
             var queryComments = from f in _context.Favorites
-                                //join o in _context.Orders on f.StoreId equals o.StoreId
-                                //join r in _context.Reviews on o.OrderId equals r.OrderId
-                                group f by f.StoreId into g
+                                join o in _context.Orders on f.StoreId equals o.StoreId
+                                join r in _context.Reviews on o.OrderId equals r.OrderId
+                                group r by r.Order.StoreId into g
                                 select new
                                 {
-                                    g.Key          
+                                    StoreId = g.Key,
+                                    avgOfRating = g.Average(re => re.ReviewRating),
+                                    countOfRating= g.Count(re => re.ReviewId != 0 ).ToString()
                                 };
+            
             ViewBag.queryComments = queryComments.ToList();
             //queryOpeningHours.ToList().FindAll(i => i.StoreId == 1);
             ViewBag.openHours = queryOpeningHours.ToList();
