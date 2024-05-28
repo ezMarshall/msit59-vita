8ee3c98fb5f5d2047db14bdf94e8e20d1e57bbb2
Marshall Wang <117267678+ezMarshall@users.noreply.github.com>
2024-05-28 12:02:39 +0800

    add image (#11)
    * init MyDataView
    
    * init MyComment View,remove MyData contaniner h-100
    
    * init FavoriteStores View
    
    * init HistoricalOrders View
    
    * init CurrentOrder View
    
    * add star image
    
    * init vitaModels, query store to FavoriteStoreView
    
    * query openHours, adjust currentOrder btn text
    
    * query openHours and comments data
    
    * cancel FavoriteStore
    
    * add comfirm for cancelFavorsite
    
    * init customerId
    
    * add store image, ,remove StorePictures files

--- msit59-vita/Controllers/MyDataController.cs
+++ msit59-vita/Controllers/MyDataController.cs
@@ -1,11 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using msit59_vita.Models;
 
 namespace msit59_vita.Controllers
 {
     public class MyDataController : Controller
     {
+        private VitaContext _context ;
+
+        private int _customerId = 1;
+
+        public MyDataController(VitaContext context) 
+        { 
+            _context = context;
+        }
+
+        /*
+         * 經使用者id取得資料
+         * 
+         */
         public IActionResult Index()
         {
+            ViewBag.CustomerID = _customerId;
+
+
+
+
+
             return View();
         }
     }
