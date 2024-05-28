53ec54b39e3ac71bed231691e082bbbc092270fc
Marshall Wang <117267678+ezMarshall@users.noreply.github.com>
2024-05-29 01:03:20 +0800

    add sql table and data (#12)
    * init MyDataView
    
    * init MyComment View,remove MyData contaniner h-100
    
    * init FavoriteStores View
    
    * init HistoricalOrders View
    
    * init CurrentOrder View
    
    * add star image
    
    * init vitaModels, query store to FavoriteStoreView
    
    * query openHours, adjust currentOrder btn text
    
    * query openHours and comments data
    
    * cancel FavoriteStore
    
    * add comfirm for cancelFavorsite
    
    * init customerId
    
    * add store image, ,remove StorePictures files
    
    * edit MyData,except address
    
    * add sql table set
    
    * add sql data

--- msit59-vita/Controllers/MyDataController.cs
+++ msit59-vita/Controllers/MyDataController.cs
@@ -5,12 +5,12 @@ namespace msit59_vita.Controllers
 {
     public class MyDataController : Controller
     {
-        private VitaContext _context ;
+        private VitaContext _context;
 
-        private int _customerId = 1;
+        private int _customerId = 3;
 
-        public MyDataController(VitaContext context) 
-        { 
+        public MyDataController(VitaContext context)
+        {
             _context = context;
         }
 
@@ -22,11 +22,56 @@ namespace msit59_vita.Controllers
         {
             ViewBag.CustomerID = _customerId;
 
+            var queryCustomerData = from c in _context.Customers
+                                    where c.CustomerId == _customerId
+                                    select new
+                                    {
+                                        c.CustomerName,
+                                        c.CustomerLocalPhone,
+                                        c.CustomerEmail,
+                                        c.CustomerEinvoiceNumber,
+                                        c.CustomerNickName,
+                                        c.CustomerId,
+                                        c.CustomerCellPhone,
+                                        c.CustomerAddressCity,
+                                        c.CustomerAddressDetails,
+                                        c.CustomerAddressDistrict
+                                    };
 
+            return View(queryCustomerData.First());
+        }
 
+        /*
+         * 修改資料
+         * 
+         * 
+         */
+        public IActionResult Edit(int id,string originPassword,string CustomerPassword, string CustomerName,string CustomerLocalPhone,string CustomerEmail,string CustomerEinvoiceNumber,string CustomerNickName,string CustomerCellPhone,string CustomerAddressCity,string CustomerAddressDetails,string CustomerAddressDistrict)
+        {
 
+            Customer customer = _context.Customers.Find(id);
+            //密碼不一樣
+            if(customer.CustomerPassword != originPassword)
+            {
+                return View();
+            }
+            if (customer == null) { 
+            
+            }
+            //必填項目
+            customer.CustomerPassword = String.IsNullOrEmpty(CustomerPassword) ? customer.CustomerPassword : CustomerPassword; 
+            customer.CustomerName = String.IsNullOrEmpty(CustomerName) ? customer.CustomerName: CustomerName;
+            //customer.CustomerNickName = String.IsNullOrEmpty(CustomerNickName) ? customer.CustomerNickName : CustomerNickName;
+            customer.CustomerNickName = CustomerNickName;
+            customer.CustomerLocalPhone = CustomerLocalPhone;
+            customer.CustomerCellPhone = CustomerCellPhone;
+            customer.CustomerAddressCity = CustomerAddressCity;
+            customer.CustomerAddressDistrict = CustomerAddressDistrict;
+            customer.CustomerAddressDetails = CustomerAddressDetails;
+            customer.CustomerEinvoiceNumber = CustomerEinvoiceNumber;
+            _context.SaveChanges();
 
-            return View();
+            return Redirect("/MyData");
         }
     }
 }
