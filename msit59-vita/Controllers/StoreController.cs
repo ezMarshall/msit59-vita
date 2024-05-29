a64fab1a1ad96e6e0f74dc5a10db411b809d22d3
whowalkingupme <46256884+whowalkingupme@users.noreply.github.com>
2024-05-29 15:22:17 +0800

    Store 更新Store資料表 (#18)
    * Models 更新
    
    * store viewdata和viewbag 用法問題修正，productCategory分類前台顯示完成
    
    * Store 營業時間和狀態顯示判斷
    
    * store 202405290134_提交
    
    * prodouct id12 資料表 image修正
    
    * sql修正撤回
    
    * Store Table 更新
    
    ---------
    
    Co-authored-by: Marshall Wang <117267678+ezMarshall@users.noreply.github.com>

--- msit59-vita/Controllers/StoreController.cs
+++ msit59-vita/Controllers/StoreController.cs
@@ -26,9 +26,14 @@ namespace msit59_vita.Controllers
 				return NotFound();
 			}
 
+			ViewData["Store"] = store;
+
+
 			var products = _context.Products
 							.Where(x => x.StoreId == id)
 							.ToList();
+
+			ViewData["Products"] = products;
 
 			var productCategoryCounts = (from product in _context.Products
 										 join category in _context.ProductCategories
@@ -44,44 +49,47 @@ namespace msit59_vita.Controllers
 
 			ViewBag.ProductCategoryCounts = productCategoryCounts;
 
-			//var productCategoryCounts = (from product in _context.Products
-			//							 join category in _context.ProductCategories
-			//							 on product.CategoryId equals category.CategoryId
-			//							 where product.StoreId == id
-			//							 group product by new { category.CategoryId, category.CategoryName } into g
-			//							 select new 
-			//							 {
-			//								 CategoryId = g.Key.CategoryId,
-			//								 CategoryName = g.Key.CategoryName,											 
-			//							 }).ToList();
-			//foreach (var item in productCategoryCounts)
-			//{
-			//	Console.WriteLine($"Category Id: {item.CategoryId}, Category Name: {item.CategoryName}, Product Count: {item.ProductCount}");
-			//}
+			///
+			//  取得店家的營業時間
+			///
+			// 獲取當前星期幾
+			string shortDayOfWeekString = DateTime.Today.DayOfWeek.ToString().Substring(0, 3);
+
+			//Console.WriteLine(shortDayOfWeekString);
 
 
-			//ViewData["ProductCategoryCounts"] = productCategoryCounts;
-			//// 獲取當前星期幾
-			//string shortDayOfWeekString = DateTime.Today.DayOfWeek.ToString().Substring(0, 3);
+			// 查詢對應的 StoreOpeningTime
+			var storeOpeningTimeQuery = from row in _context.StoreOpeningHours
+										where row.StoreId == id && row.MyWeekDay == shortDayOfWeekString
+										select new
+										{
+											myWeekDay = row.MyWeekDay,
+											storeOpenOrNot = row.StoreOpenOrNot,
+											storeOpeningTime = row.StoreOpeningTime,
+											storeClosingTime = row.StoreClosingTime,
+										};
+			var todayOpeningHours = storeOpeningTimeQuery.FirstOrDefault();
+			ViewBag.storeOpeningTimeQuery = todayOpeningHours;
 
-			////Console.WriteLine(shortDayOfWeekString);
-
-
-			//// 查詢對應的 StoreOpeningTime
-			//var storeOpeningTimeQuery = from row in _context.StoreOpeningHours
-			//							where row.StoreId == id
-			//							select new
-			//							{
-			//								myWeekDay = row.MyWeekDay,
-			//								storeOpenOrNot = row.StoreOpenOrNot,
-			//								storeOpeningTime = row.StoreOpeningTime,
-			//								storeClosingTime = row.StoreClosingTime,
-			//							};
+			var currentTime = DateTime.Now.TimeOfDay;
 
 
-			ViewData["Store"] = store;
-			ViewData["Products"] = products;
+			var orderStatusTime = 0;
+			if (todayOpeningHours.storeOpeningTime.HasValue && currentTime < todayOpeningHours.storeOpeningTime.Value.ToTimeSpan())
+			{
+				orderStatusTime = 2;
+			}
+			else if (todayOpeningHours.storeClosingTime.HasValue && currentTime > todayOpeningHours.storeClosingTime.Value.ToTimeSpan())
+			{
+				orderStatusTime = 3;
+			}
+			else
+			{
+				orderStatusTime = 4;
+			}
 
+			ViewBag.orderStatusTime = orderStatusTime;
+      
 			return View();
 		}
 	}
