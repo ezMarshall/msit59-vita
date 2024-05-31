aafabb4b808f891fd06510160d060614c67cdd77
simonwu513 <simonwu513@gmail.com>
2024-06-01 02:24:31 +0800

    ManagerStores完成

---
+++ msit59-vita/Models/ManagerProducts.cs
@@ -0,0 +1,24 @@
+﻿namespace msit59_vita.Models
+{
+    public class ManagerProducts
+    {
+        public List<ProductViewModel> Products { get; set; }
+        public List<ProductCategory> Categories { get; set; }
+        public int PageCount { get; set; }
+        public int CurrentPageIndex { get; set; }
+        public int MaxRows { get; set; }
+
+        public class ProductViewModel
+        {
+            public int ProductId { get; set; }
+            public string ProductImage { get; set; }
+            public required string ProductName { get; set; }
+            public required decimal ProductUnitPrice { get; set; }
+            public required short ProductUnitsInStock { get; set; }
+            public bool ProductOnSell { get; set; }
+            public required int CategoryId { get; set; }
+            public required string CategoryName { get; set; }
+        }
+
+    }
+}
