879cd90e3c6026eed3d287a88b79d5c61851dc1a
7rong <7ronggg@gamil.com>
2024-06-07 14:24:04 +0800

    首頁功能: google maps api距離判定&店家列表顯示

--- msit59-vita/Models/ViewModels/StoreSearchViewModel.cs
+++ msit59-vita/Models/ViewModels/StoreSearchViewModel.cs
@@ -1,6 +1,32 @@
-﻿namespace msit59_vita.Models.ViewModels
+﻿using System.Diagnostics;
+
+namespace msit59_vita.Models.ViewModels
 {
     public class StoreSearchViewModel
     {
+        public string StoreSearch { get; set; } = null!;
+
+        public int StoreId {  get; set; }
+        public string StoreName { get; set; } = null!;
+
+        public string StoreAddress { get; set; } = null!;
+
+        public string StoreImage { get; set; } = null!;
+
+        public byte StoreOrderStatus { get; set; }
+
+        public string? StoreOpeningTime { get; set; }
+
+        public bool StoreOpening { get; set; }
+
+        public decimal averageRating {  get; set; }
+
+        public int totalReviews { get; set; }
+
+        public bool isFavorite {  get; set; }
+
+        public double Dis {  get; set; }
+
+        public List<string> ProductImageList { get; set; } = null!;
     }
 }
