071328146e43bd2376b5c7e0598fdc668462a5d0
eric <whowalkingupme@gmail.com>
2024-06-09 18:40:27 +0800

    OrderDetail 功能完成

--- msit59-vita/Controllers/OrderDetailController.cs
+++ msit59-vita/Controllers/OrderDetailController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using msit59_vita.Models;
+using msit59_vita.Models.ViewModels;
 
 namespace msit59_vita.Controllers
 {
@@ -204,7 +205,130 @@ namespace msit59_vita.Controllers
 		[HttpPost]
 		public IActionResult ConfirmOrder()
 		{
+			var phoneNum = HttpContext.Session.GetString("phoneNumber");
+			var orderTime = HttpContext.Session.GetString("orderTime");
+			var pickupMethod = HttpContext.Session.GetString("pickupMethod");
+			var payMethod = HttpContext.Session.GetString("PayMethod");
+			var invoiceMethod = HttpContext.Session.GetString("invoiceMethod");
+			var carrier = HttpContext.Session.GetString("Carrier");
+			var address = HttpContext.Session.GetString("address");
+			var orderNote = HttpContext.Session.GetString("orderNote");
+
+
+			var customer = from c in _context.Customers
+						   where c.CustomerEmail == User.Identity.Name
+						   select c;
+
+			var customerId = customer.FirstOrDefault();
+
+			var customerID = customerId.CustomerId;
+
+			var cart = from c in _context.ShoppingCarts
+					   join ct in _context.Customers on c.CustomerId equals ct.CustomerId
+					   join p in _context.Products on c.ProductId equals p.ProductId
+					   join s in _context.Stores on p.StoreId equals s.StoreId
+					   where c.CustomerId == customerID
+					   select new
+					   {
+						   ShoppingCartId = c.ShoppingCartId,
+						   ShoppingCartQuantity = c.ShoppingCartQuantity,
+						   StoreId = s.StoreId,
+						   StoreName = s.StoreName,
+						   StoreLinePay = s.StoreLinePay,
+						   StorePhoneNumber = s.StorePhoneNumber,
+						   StoreUniformInvoiceVia = s.StoreUniformInvoiceVia,
+						   CustomerName = ct.CustomerName,
+						   ProductId = p.ProductId,
+						   ProductName = p.ProductName,
+						   ProductUnitPrice = p.ProductUnitPrice,
+						   ProductUnitsInStock = p.ProductUnitsInStock,
+					   };
+
+			var paymentInfo = cart.ToList();
+
+			var totalAmount = (int)paymentInfo.Sum(x => x.ProductUnitPrice * x.ShoppingCartQuantity);
+
+			DateTime currentDateTime = DateTime.Now;
+
+			DateTime predictedArrivalTime;
+
+			var orderDeliveryVia = pickupMethod=="0" ? false : true;
+			var orderPayment = payMethod == "0" ? false : true;
+
+			byte orderInvoice;
+
+			if (string.IsNullOrEmpty(invoiceMethod))
+			{
+				orderInvoice = 0; // 假設空字串或 null 對應值為 0
+			}
+			else
+			{
+				switch (invoiceMethod)
+				{
+					case "0":
+						orderInvoice = 0;
+						break;
+					case "1":
+						orderInvoice = 1;
+						break;
+					default:
+						orderInvoice = 2;
+						break;
+				}
+			}
+
+
+			string[] segments = address.Split(' ');
+
+			string addressCity = segments[0].Substring(0, Math.Min(3, segments[0].Length));
+			string addressDistrict = segments[0].Substring(Math.Min(3, segments[0].Length));
+			string addressDetails = segments.Length > 1 ? segments[1] : string.Empty;
+
+			if (DateTime.TryParse(orderTime, out predictedArrivalTime))
+			{
+				var order = new Order
+				{
+					CustomerId = customerID,
+					StoreId = paymentInfo[0].StoreId,
+					OrderTime = currentDateTime,
+					PredictedArrivalTime = predictedArrivalTime,
+					OrderDeliveryVia = orderDeliveryVia,
+					OrderPhoneNumber = phoneNum,
+					OrderStoreMemo = orderNote,
+					OrderPayment = orderPayment,
+					OrderUniformInvoiceVia = orderInvoice,
+					OrderEinvoiceNumber = carrier,
+					OrderAddressCity = addressCity,
+					OrderAddressDistrict = addressDistrict,
+					OrderAddressDetails= addressDetails,
+					CustomerOrderStatus = 0,
+
+				};
+				_context.Orders.Add(order);
+				_context.SaveChanges();
+				foreach (var item in paymentInfo)
+				{
+					var orderDetail = new OrderDetail
+					{
+						OrderId = order.OrderId,
+						ProductId = item.ProductId,
+						UnitPrice = item.ProductUnitPrice,
+						Quantity = item.ShoppingCartQuantity
+					};
+
+					_context.OrderDetails.Add(orderDetail);
+				}
+			}
+
+			
+
+
+			//刪除購物車
+			_context.ShoppingCarts.RemoveRange(_context.ShoppingCarts.Where(x => x.CustomerId == customerID));
+			_context.SaveChanges();
+
+
 			return Json(new { success = true, message = "訂單已成功完成" });
-		}	
+		}
 	}
 }
