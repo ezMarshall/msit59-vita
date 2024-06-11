0aba5b8b91ba060b8df30217fcf20043b5c8f90e
ezMarshall <e3663233@yahoo.com.tw>
2024-06-12 02:48:51 +0800

    init linepay

---
+++ msit59-vita/Controllers/LinePayController.cs
@@ -0,0 +1,54 @@
+﻿using msit59_vita.Dtos;
+using msit59_vita.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace msit59_vita.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class LinePayController : ControllerBase
+    {
+        private readonly LinePayService _linePayService;
+        public LinePayController()
+        {
+            _linePayService = new LinePayService();
+        }
+
+        [HttpPost("Create")]
+        public async Task<PaymentResponseDto> CreatePayment(PaymentRequestDto dto)
+        {
+            return await _linePayService.SendPaymentRequest(dto);
+        }
+
+        [HttpPost("Confirm")]
+        public async Task<PaymentConfirmResponseDto> ConfirmPayment([FromQuery] string transactionId, [FromQuery] string orderId, PaymentConfirmDto dto)
+        {
+            return await _linePayService.ConfirmPayment(transactionId, orderId, dto);
+        }
+
+        [HttpGet("CheckRegKey/{regKey}")]
+        public async Task<PaymentConfirmResponseDto> CheckRegKey(string regKey)
+        {
+            return await _linePayService.CheckRegKey(regKey);
+        }
+
+        [HttpPost("PayPreapproved/{regKey}")]
+        public async Task<PaymentConfirmResponseDto> PayPreapproved([FromRoute] string regKey, PayPreapprovedDto dto)
+        {
+            return await _linePayService.PayPreapproved(regKey, dto);
+        }
+
+        [HttpPost("ExpireRegKey/{regKey}")]
+        public async Task<PaymentConfirmResponseDto> ExpireRegKey(string regKey)
+        {
+            return await _linePayService.ExpireRegKey(regKey);
+        }
+
+        [HttpGet("Cancel")]
+        public async void CancelTransaction([FromQuery] string transactionId)
+        {
+            _linePayService.TransactionCancel(transactionId);
+        }
+    }
+}
+
