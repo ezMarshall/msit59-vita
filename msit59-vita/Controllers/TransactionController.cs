a66aa2e33b8c657fbbf64e83e5e93ad28813e2d5
ezMarshall <e3663233@yahoo.com.tw>
2024-06-17 10:50:11 +0800

    init TransactionRecord Model and TransactionControllers

---
+++ msit59-vita/Controllers/TransactionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using msit59_vita.Models;
+
+namespace msit59_vita.Controllers
+{
+    public class TransactionController : Controller
+    {
+
+        private readonly VitaContext _context;
+
+        public TransactionController(VitaContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<ActionResult> InitTransactionRecord() 
+        {
+            var query = from t in _context.
+
+            return await Ok();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ msit59-vita/Models/TransactionRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace msit59_vita.Models;
+
+public partial class TransactionRecord
+{
+    public int TransactionRecordId { get; set; }
+
+    public int OrderID { get; set; }
+
+    public string TransactionId { get; set; }
+
+    public DateTime TransactionTime { get; set; }
+
+    public string TransactionTimestamp { get; set; }
+
+    public byte TransactionType { get; set; }
+
+    public bool TransactionResult { get; set; }
+
+    public virtual Order Order{ get; set; } = null!;
+
+}
