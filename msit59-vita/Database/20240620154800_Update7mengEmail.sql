1995281961689ef7bcc2def2b268e4d6843c3081
7rong <128108443+7rong@users.noreply.github.com>
2024-06-20 16:37:34 +0800

    Update 7meng email (#93)
    * forgetpwd add spinner & register debug
    
    * update 7meng email
    
    * update 7meng Email
    
    ---------
    
    Co-authored-by: 7rong <7ronggg@gamil.com>

---
+++ msit59-vita/Database/20240620154800_Update7mengEmail.sql
@@ -0,0 +1,21 @@
+ USE Vita
+
+  UPDATE  [Vita].[dbo].[Customers]
+  SET CustomerEmail = '7ronggg@gmail.com'
+  WHERE CustomerEmail = '777meng@gmail.com'
+
+  UPDATE  [Vita].[dbo].[AspNetUsers]
+  SET UserName = '7ronggg@gmail.com'
+  WHERE UserName = '777meng@gmail.com'
+
+  UPDATE  [Vita].[dbo].[AspNetUsers]
+  SET Email = '7ronggg@gmail.com'
+  WHERE Email = '777meng@gmail.com'
+
+  UPDATE  [Vita].[dbo].[AspNetUsers]
+  SET NormalizedUserName = '7RONGGG@GMAIL.COM'
+  WHERE NormalizedUserName = '777MENG@GMAIL.COM'
+
+  UPDATE  [Vita].[dbo].[AspNetUsers]
+  SET NormalizedEmail = '7RONGGG@GMAIL.COM'
+  WHERE NormalizedEmail = '777MENG@GMAIL.COM'
