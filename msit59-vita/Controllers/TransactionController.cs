1407342f4eae89e8aa18894ed1109ff3ff4266cd
ezMarshall <e3663233@yahoo.com.tw>
2024-06-23 20:03:32 +0800

    init transactionRecord

--- msit59-vita/Controllers/TransactionController.cs
+++ msit59-vita/Controllers/TransactionController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using msit59_vita.Models;
+using static System.TimeZoneInfo;
 
 namespace msit59_vita.Controllers
 {
@@ -12,28 +13,31 @@ namespace msit59_vita.Controllers
         public TransactionController(VitaContext context)
         {
             _context = context;
-        }
+		}
 
-
-        public async Task<List<TransactionRecord>> InitTransactionRecord(int OrderId, string TransactionId,int TransactionType,string Timestamp,bool TransactionResult) 
+        [HttpPost]
+        public async Task<List<TransactionRecord>> InitTransactionRecord(int OrderId, string TransactionId, int TransactionType, string Timestamp, bool TransactionResult)
+        //public async Task<List<TransactionRecord>> InitTransactionRecord(int OrderId =10000422, string TransactionId = "2024062302146385910", int TransactionType = 1,string Timestamp = "1719136121956", bool TransactionResult = false)
         {
-            var query = from t in _context.TransactionRecords
-                        select t;
-
-			TransactionRecord transactionRecord = new TransactionRecord
+            TransactionRecord transactionRecord = new TransactionRecord
             {
+                //TransactionRecordsId = 1,
                 OrderId = OrderId,
                 TransactionResult = TransactionResult,
                 TransactionTime = DateTime.Now,
                 TransactionTimestamp = Timestamp,
-                TransactionType= (byte)TransactionType,
+                TransactionType = (byte)TransactionType,
                 TransactionId = TransactionId
 
-			};
-
+            };
 
             _context.TransactionRecords.Add(transactionRecord);
             _context.SaveChanges();
+
+
+            var query = from t in _context.TransactionRecords
+                        where t.TransactionResult == TransactionResult && t.TransactionId == TransactionId
+                        select t;
 			return await query.ToListAsync();
         }
 
